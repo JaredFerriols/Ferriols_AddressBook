a1ce1f9825692d93e89c42d85fa3c3b974125702
JaredFerriols <JNoeF01@gmail.com>
2024-02-21 16:20:53 -0800

    Fixed unit test not being able to be commited, but they are in the out directory.

---
+++ src/test/address/MenuTest.java
@@ -0,0 +1,161 @@
+package address.data;
+
+import address.data.AddressBook;
+import address.data.AddressEntry;
+import address.Menu;
+import org.junit.jupiter.api.Test;
+
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class is used to test the Menu class to make sure it works properly.
+ * @author Jared Ferriols
+ * @since JDK 17
+ * @version 1.2
+ */
+
+public class MenuTest {
+
+
+    // https://stackoverflow.com/questions/6415728/junit-testing-with-simulated-user-input
+    // https://stackoverflow.com/questions/3894798/hide-java-output
+    /**
+     * Test method for {@link Menu#prompt_FirstName()}
+     */
+    @Test
+    public void testPrompt_FirstName() {
+        InputStream sysInBackup = System.in;
+        String firstName = "John";
+        ByteArrayInputStream input = new ByteArrayInputStream(firstName.getBytes());
+        System.setIn(input);
+        assertEquals(firstName, Menu.prompt_FirstName());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_LastName()}
+     */
+    @Test
+    public void testPrompt_LastName() {
+        InputStream sysInBackup = System.in;
+        String lastName = "Smith";
+        ByteArrayInputStream input = new ByteArrayInputStream(lastName.getBytes());
+        System.setIn(input);
+        assertEquals(lastName, Menu.prompt_LastName());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_Street()}
+     */
+    @Test
+    public void testPrompt_Street() {
+        InputStream sysInBackup = System.in;
+        String street = "1234 Address St";
+        ByteArrayInputStream input = new ByteArrayInputStream(street.getBytes());
+        System.setIn(input);
+        assertEquals(street, Menu.prompt_Street());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_City()}
+     */
+    @Test
+    public void testPrompt_City() {
+        InputStream sysInBackup = System.in;
+        String city = "Hayward";
+        ByteArrayInputStream input = new ByteArrayInputStream(city.getBytes());
+        System.setIn(input);
+        assertEquals(city, Menu.prompt_City());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_State()}
+     */
+    @Test
+    public void testPrompt_State() {
+        InputStream sysInBackup = System.in;
+        String state = "CA";
+        ByteArrayInputStream input = new ByteArrayInputStream(state.getBytes());
+        System.setIn(input);
+        assertEquals(state, Menu.prompt_State());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_Zip()}
+     */
+    @Test
+    public void testPrompt_Zip() {
+        InputStream sysInBackup = System.in;
+        String zip = "94586";
+        ByteArrayInputStream input = new ByteArrayInputStream(zip.getBytes());
+        System.setIn(input);
+        assertEquals(Integer.parseInt(zip), Menu.prompt_Zip());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_Telephone()}
+     */
+    @Test
+    public void testPrompt_Telephone() {
+        InputStream sysInBackup = System.in;
+        String telephone = "510-555-1234";
+        ByteArrayInputStream input = new ByteArrayInputStream(telephone.getBytes());
+        System.setIn(input);
+        assertEquals(telephone, Menu.prompt_Telephone());
+        System.setIn(sysInBackup);
+    }
+
+    /**
+     * Test method for {@link Menu#prompt_Email()}
+     */
+    @Test
+    public void testPrompt_Email() {
+        InputStream sysInBackup = System.in;
+        String email = "best1@gmail.com";
+        ByteArrayInputStream input = new ByteArrayInputStream(email.getBytes());
+        System.setIn(input);
+        assertEquals(email, Menu.prompt_Email());
+        System.setIn(sysInBackup);
+    }
+
+    // https://stackoverflow.com/questions/246038/unit-testing-void-methods
+    /**
+     * Test method for {@link Menu#displayMenu()}
+     */
+    @Test
+    public void testDisplayMenu() {
+
+        ByteArrayOutputStream output1 = new ByteArrayOutputStream();
+        PrintStream ps1 = new PrintStream(output1);
+        String expected1 = """
+                *************************
+                Please enter your menu selection
+                a) Loading From File
+                b) Addition
+                c) Removal
+                d) Find
+                e) Listing
+                f) Quit
+                *************************
+                """;
+
+        PrintStream old = System.out;
+        System.setOut(ps1);
+
+        Menu.displayMenu();
+
+        System.out.flush();
+        System.setOut(old);
+
+        assertEquals(expected1, output1.toString());
+
+    }
+}
---
+++ src/test/address/data/AddressBookTest.java
@@ -0,0 +1,247 @@
+package address;
+
+import address.data.AddressBook;
+import address.data.AddressEntry;
+import org.junit.jupiter.api.Test;
+import java.io.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * This class is used to test the AddressBook class to make sure it works properly.
+ * @author Jared Ferriols
+ * @since JDK 17
+ * @version 1.2
+ */
+public class AddressBookTest {
+
+    /**
+     * A preloaded object of AddressEntry to load into AddressBook
+     */
+    private static AddressEntry makeEntryA() {
+        return new AddressEntry("John", "Smith", "1234 Address St", "Union City", "CA", 94587, "best1@gmail.com", "111-234-5678");
+    }
+    /**
+     * A preloaded object of AddressEntry to load into AddressBook
+     */
+    private static AddressEntry makeEntryB() {
+        return new AddressEntry("Adrian", "Lester", "4321 Address St", "Fremont", "CA", 94536, "best2@gmail.com", "999-876-5432");
+    }
+    /**
+     * A preloaded object of AddressEntry to load into AddressBook
+     */
+    private static AddressEntry makeEntryC() {
+        return new AddressEntry("Mary", "Smith", "5678 Address St", "San Francisco", "CA", 94101, "best3@gmail.com", "510-555-1234");
+    }
+
+
+    /**
+     * Test method for {@link AddressBook#list()}
+     */
+    @org.junit.jupiter.api.Test
+    public void testList() {
+        AddressBook ab = new AddressBook();
+        ByteArrayOutputStream output1 = new ByteArrayOutputStream();
+        ByteArrayOutputStream output2 = new ByteArrayOutputStream();
+        PrintStream ps1 = new PrintStream(output1);
+        PrintStream ps2 = new PrintStream(output2);
+
+        ab.add(makeEntryA());
+        String expected1 = "";
+        expected1 += "1: John, Smith\n\t1234 Address St\n\tUnion City, CA 94587\n\t111-234-5678\n\tbest1@gmail.com\n";
+
+
+        PrintStream old1 = System.out;
+        System.setOut(ps1);
+
+        ab.list();
+        System.out.flush();
+        System.setOut(old1);
+
+        assertEquals(expected1, output1.toString());
+
+        ab.add(makeEntryB());
+        String expected2 = "";
+        expected2 += "1: Adrian, Lester\n\t4321 Address St\n\tFremont, CA 94536\n\t999-876-5432\n\tbest2@gmail.com\n";
+        expected2 += "2: John, Smith\n\t1234 Address St\n\tUnion City, CA 94587\n\t111-234-5678\n\tbest1@gmail.com\n";
+
+        PrintStream old2 = System.out;
+        System.setOut(ps2);
+        ab.list();
+        System.out.flush();
+        System.setOut(old2);
+
+        assertEquals(expected2, output2.toString());
+    }
+
+    /**
+     * Test method for {@link AddressBook#add(AddressEntry)}
+     */
+    @org.junit.jupiter.api.Test
+    public void testAdd() {
+        AddressBook ab = new AddressBook();
+        ByteArrayOutputStream output1 = new ByteArrayOutputStream();
+        ByteArrayOutputStream output2 = new ByteArrayOutputStream();
+        PrintStream ps1 = new PrintStream(output1);
+        PrintStream ps2 = new PrintStream(output2);
+
+        ab.add(makeEntryC());
+        String expected1 = "";
+        expected1 += "1: Mary, Smith\n\t5678 Address St\n\tSan Francisco, CA 94101\n\t510-555-1234\n\tbest3@gmail.com\n";
+
+
+
+        /*
+        expected1 += "1: Adrian, Lester\n\t4321 Address St\n\tFremont, CA 94536\n\t999-876-5432\n\tbest2@gmail.com\n";
+        expected1 += "2: John, Smith\n\t1234 Address St\n\tUnion City, CA 94587\n\t111-234-5678\n\tbest1@gmail.com\n";
+        expected1 += "3: Mary, Smith\n\t5678 Address St\n\tSan Francisco, CA 94101\n\t510-555-1234\n\tbest3@gmail.com\n";
+         */
+        PrintStream old1 = System.out;
+        System.setOut(ps1);
+
+        ab.list();
+        System.out.flush();
+        System.setOut(old1);
+
+        assertEquals(expected1, output1.toString());
+
+        ab.add(makeEntryB());
+        String expected2 = "";
+        expected2 += "1: Adrian, Lester\n\t4321 Address St\n\tFremont, CA 94536\n\t999-876-5432\n\tbest2@gmail.com\n";
+        expected2 += "2: Mary, Smith\n\t5678 Address St\n\tSan Francisco, CA 94101\n\t510-555-1234\n\tbest3@gmail.com\n";
+
+        PrintStream old2 = System.out;
+        System.setOut(ps2);
+        ab.list();
+        System.out.flush();
+        System.setOut(old2);
+
+        assertEquals(expected2, output2.toString());
+    }
+
+    /**
+     * Test method for {@link AddressBook#loadEntriesFromFile(java.lang.String)}
+     */
+    @org.junit.jupiter.api.Test
+    public void testLoadEntriesFromFile() {
+        AddressBook ab = new AddressBook();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(output);
+
+        ab.loadEntriesFromFile("testFile.txt");
+        String expected1 = "";
+        expected1 += "1: Adrian, Lester\n\t4321 Address St\n\tFremont, CA 94536\n\t999-876-5432\n\tbest2@gmail.com\n";
+        expected1 += "2: John, Smith\n\t1234 Address St\n\tUnion City, CA 94587\n\t111-234-5678\n\tbest1@gmail.com\n";
+        expected1 += "3: Mary, Smith\n\t5678 Address St\n\tSan Francisco, CA 94101\n\t510-555-1234\n\tbest3@gmail.com\n";
+
+        PrintStream old = System.out;
+        System.setOut(ps);
+        String input1 = "y";
+        ByteArrayInputStream input2 = new ByteArrayInputStream(input1.getBytes());
+        System.setIn(input2);
+        ab.list();
+        System.out.flush();
+        System.setOut(old);
+
+        assertEquals(expected1, output.toString());
+
+    }
+
+    // https://stackoverflow.com/questions/8708342/redirect-console-output-to-string-in-java
+    // https://www.programiz.com/java-programming/bytearrayoutputstream
+    /**
+     * Test method for {@link AddressBook#remove(java.lang.String)}
+     */
+    @org.junit.jupiter.api.Test
+    public void testRemove() {
+        AddressBook ab = new AddressBook();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(output);
+        ab.add(makeEntryB());
+        String result1 = "The following entry was found in the address book.\r\n" +
+                "   " + makeEntryB().toString() + "\n" + "Hit 'y' to remove the entry or 'n' to return to main menu\r\n";
+        String input1 = "y";
+        ByteArrayInputStream input2 = new ByteArrayInputStream(input1.getBytes());
+        System.setIn(input2);
+        PrintStream old = System.out;
+        System.setOut(ps);
+        ab.remove("L");
+        System.out.flush();
+        System.setOut(old);
+        assertEquals(result1, output.toString());
+
+
+
+    }
+
+    /**
+     * Test method for {@link AddressBook#find(java.lang.String)}
+     */
+    @org.junit.jupiter.api.Test
+    public void testFind() {
+        AddressBook ab = new AddressBook();
+        ab.loadEntriesFromFile("testFile.txt");
+        ByteArrayOutputStream output1 = new ByteArrayOutputStream();
+        ByteArrayOutputStream output2 = new ByteArrayOutputStream();
+        ByteArrayOutputStream output3 = new ByteArrayOutputStream();
+        ByteArrayOutputStream output4 = new ByteArrayOutputStream();
+        PrintStream ps1 = new PrintStream(output1);
+        PrintStream ps2 = new PrintStream(output2);
+        PrintStream ps3 = new PrintStream(output3);
+        PrintStream ps4 = new PrintStream(output4);
+
+        String input1 = "";
+        String input2 = "A";
+        String input3 = "L";
+        String input4 = "S";
+
+        String result1 = "The following 3 entries were found in the address book" +
+                " for a last name starting with \"\"\r\n" + "1: " + makeEntryB().toString() + "\n\n"
+                + "2: " + makeEntryA().toString() + "\n\n" + "3: " + makeEntryC().toString() + "\n\n";
+
+        // output1.reset();
+        String result2 = "There were no entries were found in the address book" +
+                " for a last name starting with " + "\"" + input2 + "\"\r\n";
+        String result3 = "The following 1 entries were found in the address book" +
+                " for a last name starting with " + "\"" + input3 + "\"\r\n" + "1: " + makeEntryB().toString() + "\n\n";
+        String result4 = "The following 2 entries were found in the address book" +
+                " for a last name starting with " + "\"" + input4 + "\"\r\n" + "1: " + makeEntryA().toString() + "\n\n"
+                + "2: " + makeEntryC().toString() + "\n\n";
+
+
+        PrintStream old1 = System.out;
+        System.setOut(ps1);
+        ab.find(input1);
+        assertEquals(result1, output1.toString());
+        System.out.flush();
+        System.setOut(old1);
+        // output1.reset();
+
+        PrintStream old2 = System.out;
+        System.setOut(ps2);
+        ab.find(input2);
+        assertEquals(result2, output2.toString());
+        System.out.flush();
+        System.setOut(old2);
+        // output1.reset();
+
+        PrintStream old3 = System.out;
+        System.setOut(ps3);
+        ab.find(input3);
+        assertEquals(result3, output3.toString());
+        System.out.flush();
+        System.setOut(old3);
+        // output1.reset();
+
+        PrintStream old4 = System.out;
+        System.setOut(ps4);
+        ab.find(input4);
+        assertEquals(result4, output4.toString());
+        System.out.flush();
+        System.setOut(old4);
+        // output1.reset();
+
+    }
+
+}
